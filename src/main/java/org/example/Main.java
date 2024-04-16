ef07242a83355988a65556f8a84ccf7b81441d41
Max <dalannok@gmail.com>
2024-04-16 10:01:13 +0300

    Changed main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         int storageCapacity = 3;
-        //int itemCount = 10;
+        int itemCount = 5;
+        int producerConsumerCount = 2;
 
         Storage storage = new Storage(storageCapacity);
-        Thread producerThread = new Thread(new Producer(storage, 7));
-        Thread consumerThread = new Thread(new Consumer(storage, 7));
-
-        Thread producerThread1 = new Thread(new Producer(storage, 3));
-        Thread consumerThread1 = new Thread(new Consumer(storage, 3));
-
+        for(int i = 0; i < producerConsumerCount; i++){
+            Thread consumerThread = new Thread(new Consumer(storage, itemCount));
+            Thread producerThread = new Thread(new Producer(storage, itemCount));
+        }
     }
 }
